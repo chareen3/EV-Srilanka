0c83c3d086f0f2424cf27b4a8b3a7d96c24469f0
Charith <chareenmacno@gmail.com>
2024-04-01 21:56:55 +0530

    EVLanka

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'poppins': require('./assets/fonts/Poppins-Regular.ttf'),
+    'poppins-semibold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontSize:20,
+        fontFamily:'poppins'
+        }}>Hello Chareen</Text>
       <StatusBar style="auto" />
     </View>
   );
